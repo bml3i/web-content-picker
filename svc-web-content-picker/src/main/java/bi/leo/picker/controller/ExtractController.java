5538b19fb6b91de547540ec4f0019f0ecc814bd0
Leo Bi <biminglei@gmail.com>
2023-04-24 15:55:31 +0800

    update

--- svc-web-content-picker/src/main/java/bi/leo/picker/controller/ExtractController.java
+++ svc-web-content-picker/src/main/java/bi/leo/picker/controller/ExtractController.java
@@ -5,6 +5,8 @@ import bi.leo.picker.model.ExtractRequest;
 import bi.leo.picker.model.ExtractResult;
 import bi.leo.picker.model.ResponseResult;
 import bi.leo.picker.service.ExtractService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,26 @@ public class ExtractController {
     @Autowired
     ExtractService extractService;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
+    @GetMapping(path = "/user")
+    @ResponseBody
+    public ObjectNode getUserInfo() {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("name", "Frank");
+        objectNode.put("age", 24);
+        return objectNode;
+    }
+
+    @CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
     @RequestMapping(value = "/extract", method=RequestMethod.POST)
     public ResponseEntity extract(@RequestBody ExtractRequest extractRequest) throws CustomWebDriverException {
 
